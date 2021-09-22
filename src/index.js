d3d37f690bdb19ffe72046d5c90043e772d8b0d1
Elian Doran <elinnum@gmail.com>
2021-09-22 17:24:23 +0300

    Set up entry point for the application

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+import PoChecker from "../lib/checks/po/po.js";
+
+function getConfiguration() {
+  const configPath = "config.json";
+  const fileContent = fs.readFileSync(configPath).toString("utf-8");
+  return JSON.parse(fileContent);
+}
+
+function main() {
+  const config = getConfiguration();
+  const context = {
+    baseDir: config.baseDir
+  };
+
+  const poCheck = new PoChecker(context);
+  poCheck.startCheck();
+}
+
+main();
