8e56d744d33ffa35ac5ffc46d02b1a33b97415fb
Elian Doran <contact@eliandoran.me>
2024-02-09 18:24:58 +0200

    log-analyzer: Forward exit code

--- analyze.sh
+++ analyze.sh
@@ -10,7 +10,10 @@ LOG_FILE=input.log
 
 # Execute the given command, preserving its output but also logging to a file
 # for later analysis.
-$@ 2>&1 | tee "$LOG_FILE"
+"$@" 2>&1 | tee "$LOG_FILE"
+
+exit_code=${PIPESTATUS[0]}
 
 # Run the analysis
 cat "$LOG_FILE" | node index.js
+exit $exit_code
