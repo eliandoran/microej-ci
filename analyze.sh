b901194919fc9e69f434719e1b091a3a38bb0999
Elian Doran <contact@eliandoran.me>
2024-02-09 18:10:01 +0200

    log-analyzer: Create script to proxy logs

--- analyze.sh
+++ analyze.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <command>"
+    echo "Consult the README for information."
+    exit 1
+fi
+
+# Execute the given command, preserving its output but also logging to a file
+# for later analysis.
+$@ 2>&1 | tee input.log
